deaf427c63658081862e851779fea8c5f99fd70d
Mokheon Park <mokimokheonpark@gmail.com>
2024-03-06 18:05:51 -0500

    connected MongoDB for the project

--- app/page.js
+++ app/page.js
@@ -1,3 +1,13 @@
-export default function Home() {
-  return <div>Hello</div>;
+import { connectDB } from "/util/database.js";
+
+export default async function Home() {
+  const client = await connectDB;
+  const db = client.db("Bulletin-Board");
+  const dbTest = await db.collection("post").find().toArray();
+  return (
+    <div>
+      <h1>{dbTest[0].title}</h1>
+      <h2>{dbTest[0].content}</h2>
+    </div>
+  );
 }
