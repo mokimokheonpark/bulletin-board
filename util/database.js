0e54c770c9b5775412a73f4fc7ea8747241c928e
Mokheon Park <mokimokheonpark@gmail.com>
2024-03-12 15:26:25 -0400

    removed useNewUrlParser option in database

--- util/database.js
+++ util/database.js
@@ -1,15 +1,16 @@
 import { MongoClient } from "mongodb";
+
 const url =
   "mongodb+srv://mokimokheonpark:moki123@cluster0.f8wvtex.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
-const options = { useNewUrlParser: true };
 let connectDB;
 
 if (process.env.NODE_ENV === "development") {
   if (!global._mongo) {
-    global._mongo = new MongoClient(url, options).connect();
+    global._mongo = new MongoClient(url).connect();
   }
   connectDB = global._mongo;
 } else {
-  connectDB = new MongoClient(url, options).connect();
+  connectDB = new MongoClient(url).connect();
 }
+
 export { connectDB };
