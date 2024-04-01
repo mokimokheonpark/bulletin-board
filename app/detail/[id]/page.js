c14118d7d0b64febff3c62c78368e7a4145d85f6
Mokheon Park <mokimokheonpark@gmail.com>
2024-03-31 23:47:07 -0400

    session check for adding a comment

--- app/detail/[id]/page.js
+++ app/detail/[id]/page.js
@@ -1,6 +1,9 @@
+import { getServerSession } from "next-auth";
 import { ObjectId } from "mongodb";
 import { connectDB } from "@/util/database";
 import Comment from "@/app/components/Comment";
+import LogInBtn from "@/app/components/LogInBtn";
+import { authOptions } from "@/pages/api/auth/[...nextauth]";
 
 export default async function Detail(props) {
   const client = await connectDB;
@@ -8,12 +11,20 @@ export default async function Detail(props) {
   const postDatum = await db
     .collection("post")
     .findOne({ _id: new ObjectId(props.params.id) });
+  const session = await getServerSession(authOptions);
 
   return (
-    <div>
+    <div className="p-20">
       <h4>{postDatum.title}</h4>
       <p>{postDatum.content}</p>
-      <Comment postDatumId={postDatum._id.toString()} />
+      {!session ? (
+        <div>
+          <h4>To add a comment, please log in first.</h4>
+          <LogInBtn />
+        </div>
+      ) : (
+        <Comment postDatumId={postDatum._id.toString()} />
+      )}
     </div>
   );
 }
--- app/message/page.js
+++ app/message/page.js
@@ -3,7 +3,7 @@ import LogInBtn from "../components/LogInBtn";
 export default async function Message() {
   return (
     <div className="p-20">
-      <h2>Please log in first.</h2>
+      <h2>To write a post, please log in first.</h2>
       <LogInBtn />
     </div>
   );
