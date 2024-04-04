b65fe246bf74d8813711b6eea934514cbf7f4117
Mokheon Park <mokimokheonpark@gmail.com>
2024-04-04 18:04:42 -0400

    store uploaded image url in database

--- app/write/page.js
+++ app/write/page.js
@@ -1,27 +1,63 @@
-import { getServerSession } from "next-auth";
-import LogInBtn from "../components/LogInBtn";
-import UploadImage from "../components/UploadImage";
-import { authOptions } from "@/pages/api/auth/[...nextauth]";
+"use client";
 
-export default async function Write() {
-  const session = await getServerSession(authOptions);
+import { useState } from "react";
+
+export default function Write() {
+  const [imageUrl, setImageUrl] = useState("");
+  const [isUploading, setIsUploading] = useState(false);
 
   return (
     <div className="p-20">
-      {!session ? (
-        <div>
-          <h2>To write a post, please log in first.</h2>
-          <LogInBtn />
-        </div>
+      <h4>WRITE</h4>
+      <form action="/api/post/write" method="POST">
+        <input name="title" placeholder="Title" required />
+        <input name="content" placeholder="Content" required />
+        <input
+          type="file"
+          accept="image/*"
+          onChange={async (e) => {
+            setIsUploading(true);
+            const file = e.target.files[0];
+            if (file) {
+              const fileName = encodeURIComponent(file.name);
+              const response = await fetch(
+                `/api/post/get-presigned-url?file=${fileName}`
+              );
+              const result = await response.json();
+              const formData = new FormData();
+              Object.entries({ ...result.fields, file }).forEach(
+                ([key, value]) => {
+                  formData.append(key, value);
+                }
+              );
+              const postResponse = await fetch(result.url, {
+                method: "POST",
+                body: formData,
+              });
+              if (postResponse.ok) {
+                setImageUrl(postResponse.url + "/" + fileName);
+              }
+            } else {
+              setImageUrl("");
+            }
+            setIsUploading(false);
+          }}
+          disabled={isUploading}
+        />
+        <input type="hidden" name="image-url" value={imageUrl} />
+        <button type="submit" disabled={isUploading}>
+          Post
+        </button>
+      </form>
+      {isUploading ? (
+        <p>Uploading the image...</p>
       ) : (
         <div>
-          <h4>WRITE</h4>
-          <form action="/api/post/write" method="POST">
-            <input name="title" placeholder="Title" required />
-            <input name="content" placeholder="Content" required />
-            <UploadImage />
-            <button type="submit">Post</button>
-          </form>
+          {imageUrl ? <p>Image Preview</p> : null}
+          <img
+            src={imageUrl}
+            style={{ maxWidth: "300px", maxHeight: "300px" }}
+          />
         </div>
       )}
     </div>
--- pages/api/post/get-presigned-url.js
+++ pages/api/post/get-presigned-url.js
@@ -1,6 +1,6 @@
 import aws from "aws-sdk";
 
-export default async function handler(res, req) {
+export default async function handler(req, res) {
   aws.config.update({
     accessKeyId: process.env.S3_ACCESS_KEY_ID,
     secretAccessKey: process.env.S3_SECRET_KEY_ID,
@@ -11,10 +11,10 @@ export default async function handler(res, req) {
   const s3 = new aws.S3();
   const url = await s3.createPresignedPost({
     Bucket: process.env.S3_BUCKET_NAME,
-    Fields: { key: res.query.file },
+    Fields: { key: req.query.file },
     Expires: 60,
     Conditions: [["content-length-range", 0, 5242880]],
   });
 
-  req.status(200).json(url);
+  res.status(200).json(url);
 }
