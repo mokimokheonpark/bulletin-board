998941ebaf5fb5883ea0c575521f1fd6830cab50
Mokheon Park <mokimokheonpark@gmail.com>
2024-04-08 18:13:24 -0400

    modified some styles in ListItem component

--- app/components/ListItem.js
+++ app/components/ListItem.js
@@ -8,17 +8,19 @@ export default function ListItem(props) {
       {props.postData.map((item, index) => {
         return (
           <div className="list-item" key={index}>
-            <Link href={`/detail/${item._id}`} prefetch={false}>
-              <h4>{item.title}</h4>
-            </Link>
-            {props.session && props.session.user.email === item.userEmail ? (
-              <div>
-                <Link href={`/edit/${item._id}`}>
-                  <MdEdit />
-                </Link>
-                <DeletePost postDatumId={item._id.toString()} />
-              </div>
-            ) : null}
+            <div className="list-item-div">
+              <Link href={`/detail/${item._id}`} prefetch={false}>
+                <h4>{item.title}</h4>
+              </Link>
+              {props.session && props.session.user.email === item.userEmail ? (
+                <div>
+                  <Link href={`/edit/${item._id}`}>
+                    <MdEdit />
+                  </Link>
+                  <DeletePost postDatumId={item._id.toString()} />
+                </div>
+              ) : null}
+            </div>
             <p>{item.username}</p>
           </div>
         );
