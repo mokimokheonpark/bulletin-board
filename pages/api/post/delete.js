c3f7775967356bbf0eeeaa7d1d148bcbbb13a2b9
Mokheon Park <mokimokheonpark@gmail.com>
2024-04-09 14:21:50 -0400

    when deleting a post, all the comments on the post are also deleted from DB

--- pages/api/post/delete.js
+++ pages/api/post/delete.js
@@ -7,6 +7,9 @@ export default async function handler(req, res) {
       const client = await connectDB;
       const db = client.db("Bulletin-Board");
       await db.collection("post").deleteOne({ _id: new ObjectId(req.body) });
+      await db
+        .collection("comment")
+        .deleteMany({ postDatumId: new ObjectId(req.body) });
       res.status(200).json("Successfully deleted");
     } catch (error) {
       res.status(500).json(error);
