079abf9f1045153a514e120e8ee479d35437a4a2
Mokheon Park <mokimokheonpark@gmail.com>
2024-04-15 15:05:20 -0400

    debugging: editing username applied to all the posts and comments of the user

--- pages/api/username/edit.js
+++ pages/api/username/edit.js
@@ -5,13 +5,24 @@ export default async function handler(req, res) {
     if (req.body.username === "") {
       return res.status(500).json("Username is required.");
     }
-    const updatedData = { username: req.body.username };
+    const updatedUserData = { username: req.body.username };
+    const updatedPostData = { username: req.body.username };
+    const updatedCommentData = { commenterUsername: req.body.username };
     try {
       const client = await connectDB;
       const db = client.db("Bulletin-Board");
       await db
         .collection("user")
-        .updateOne({ email: req.body.email }, { $set: updatedData });
+        .updateOne({ email: req.body.email }, { $set: updatedUserData });
+      await db
+        .collection("post")
+        .updateMany({ userEmail: req.body.email }, { $set: updatedPostData });
+      await db
+        .collection("comment")
+        .updateMany(
+          { commenterEmail: req.body.email },
+          { $set: updatedCommentData }
+        );
       res.redirect(302, "/profile");
     } catch (error) {
       res.status(500).json(error);
