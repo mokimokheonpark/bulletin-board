327d74b2d47d0d9055fec7c7e84666a4c45ddb76
Mokheon Park <mokimokheonpark@gmail.com>
2024-04-17 19:44:31 -0400

    updated DeletePost component: a warning message will be shown when trying to delete a post

--- app/components/DeletePost.js
+++ app/components/DeletePost.js
@@ -5,32 +5,47 @@ import { usePathname, useRouter } from "next/navigation";
 import { MdDelete } from "react-icons/md";
 
 export default function DeletePost(props) {
+  const [showConfirmation, setShowConfirmation] = useState(false);
   const [isDeleting, setIsDeleting] = useState(false);
   const pathname = usePathname();
   const router = useRouter();
+  const handleDelete = async () => {
+    setIsDeleting(true);
+    await fetch("/api/post/delete", {
+      method: "DELETE",
+      body: props.postDatumId,
+    });
+    setIsDeleting(false);
+    setShowConfirmation(false);
+    if (pathname === "/list") {
+      router.replace("/list", { scroll: false });
+    } else if (pathname === "/my-posts") {
+      router.replace("/my-posts", { scroll: false });
+    } else {
+      router.replace("/deleted");
+    }
+    router.refresh();
+  };
 
   return (
-    <span
-      className="cursor-pointer"
-      onClick={async () => {
-        setIsDeleting(true);
-        await fetch("/api/post/delete", {
-          method: "DELETE",
-          body: props.postDatumId,
-        });
-        setIsDeleting(false);
-        if (pathname === "/list") {
-          router.replace("/list", { scroll: false });
-        } else if (pathname === "/my-posts") {
-          router.replace("/my-posts", { scroll: false });
-        } else {
-          router.replace("/deleted");
-        }
-        router.refresh();
-      }}
-    >
-      <MdDelete />
-      {isDeleting ? <span> Deleting the post...</span> : null}
+    <span>
+      {showConfirmation ? (
+        <span>
+          Are you sure you want to delete this post?{" "}
+          <button onClick={handleDelete}>Yes</button>{" "}
+          <button onClick={() => setShowConfirmation(false)}>No</button>
+          {isDeleting ? <span> Deleting the post...</span> : null}
+        </span>
+      ) : (
+        <span
+          className="cursor-pointer"
+          onClick={async () => {
+            setShowConfirmation(true);
+          }}
+        >
+          <MdDelete />
+        </span>
+      )}
     </span>
   );
 }
