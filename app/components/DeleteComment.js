b4c244b615b2f4bc599baecf6f5ed13dc3264360
Mokheon Park <mokimokheonpark@gmail.com>
2024-04-18 00:50:51 -0400

    updated DeleteComment component: a warning message will be shown when trying to delete a comment

--- app/components/DeleteComment.js
+++ app/components/DeleteComment.js
@@ -5,24 +5,39 @@ import { useRouter } from "next/navigation";
 import { MdDelete } from "react-icons/md";
 
 export default function DeleteComment(props) {
+  const [showConfirmation, setShowConfirmation] = useState(false);
   const [isDeleting, setIsDeleting] = useState(false);
   const router = useRouter();
+  const handleDelete = async () => {
+    setIsDeleting(true);
+    await fetch("/api/comment/delete", {
+      method: "DELETE",
+      body: props.commentDatumId,
+    });
+    setIsDeleting(false);
+    setShowConfirmation(false);
+    router.refresh();
+  };
 
   return (
-    <span
-      className="cursor-pointer"
-      onClick={async () => {
-        setIsDeleting(true);
-        await fetch("/api/comment/delete", {
-          method: "DELETE",
-          body: props.commentDatumId,
-        });
-        setIsDeleting(false);
-        router.refresh();
-      }}
-    >
-      <MdDelete />
-      {isDeleting ? <span> Deleting the comment...</span> : null}
+    <span>
+      {showConfirmation ? (
+        <span>
+          Are you sure you want to delete this comment?{" "}
+          <button onClick={handleDelete}>Yes</button>{" "}
+          <button onClick={() => setShowConfirmation(false)}>No</button>
+          {isDeleting ? <span> Deleting the comment...</span> : null}
+        </span>
+      ) : (
+        <span
+          className="cursor-pointer"
+          onClick={async () => {
+            setShowConfirmation(true);
+          }}
+        >
+          <MdDelete />
+        </span>
+      )}
     </span>
   );
 }
