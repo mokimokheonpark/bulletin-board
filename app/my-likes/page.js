4d8766e5071208e9dbd36de1f87eeb4ef582ac06
Mokheon Park <mokimokheonpark@gmail.com>
2024-04-22 15:43:09 -0400

    implemented my-likes page

---
+++ app/my-likes/page.js
@@ -0,0 +1,44 @@
+import { getServerSession } from "next-auth";
+import { ObjectId } from "mongodb";
+import ListItem from "../components/ListItem";
+import { authOptions } from "@/pages/api/auth/[...nextauth]";
+import { connectDB } from "@/util/database";
+
+export const dynamic = "force-dynamic";
+
+export default async function MyLikes() {
+  const session = await getServerSession(authOptions);
+  const client = await connectDB;
+  const db = client.db("Bulletin-Board");
+  const userDatum = await db
+    .collection("user")
+    .findOne({ email: session.user.email });
+  const likesIds = userDatum.likes;
+  const likesObjectIds = [];
+  for (let i = 0; i < likesIds.length; i++) {
+    likesObjectIds.push(new ObjectId(likesIds[i]));
+  }
+  const myLikedPostData = await db
+    .collection("post")
+    .find({ _id: { $in: likesObjectIds } })
+    .toArray();
+
+  return (
+    <div className="list-bg">
+      <div className="p-20">
+        <h2>My liked posts</h2>
+      </div>
+      {myLikedPostData.length > 0 ? (
+        <ListItem
+          session={session}
+          postData={myLikedPostData}
+          userDatum={userDatum}
+        />
+      ) : (
+        <div className="p-20">
+          <h2>You have not liked any posts yet.</h2>
+        </div>
+      )}
+    </div>
+  );
+}
